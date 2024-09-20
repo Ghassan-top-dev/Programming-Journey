06c9308a33033a5905fc6775f09283523ea32c24
Ghassan Murad Agha <gmuradag@uoguelph.ca>
2024-09-19 23:17:41 -0400

    Created final_digit.c
    This is a basic version of a program I am working on which takes an integer from the user and returns the final digit of that integer considering the inputted integer is a whole number. I am working on making this program accept floats as well.

---
+++ final_digit.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+
+    int userInput; 
+    int lastDigit;
+
+    printf("Enter your number and I will return the final digit of the number: ");
+
+    scanf("%d", &userInput); 
+
+    lastDigit = userInput % 10; 
+
+    printf("your last digit is: %d", lastDigit); 
+
+
+    return 0;
+}
