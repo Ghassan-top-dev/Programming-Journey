911d5fe747dd5dca6f1f9d8f22426b9e6f67d0d3
Ghassan Murad Agha <gmuradag@uoguelph.ca>
2024-09-19 23:32:10 -0400

    Created char.c
    this program checks weather the user input is a digit or not

---
+++ char.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main()
+{
+    char input;
+
+    printf("Enter a character: ");
+    scanf("%c",&input);
+
+    if (isdigit(input) == 0)
+         printf("%c is not a digit.",input);
+    else
+         printf("%c is a digit.",input);
+    return 0;
+}
