dd1cf0f65a7dbc2b3edb69100a0ddcb532cdc66b
Ghassan Murad Agha <gmuradag@uoguelph.ca>
2024-10-13 20:10:06 -0400

    Updated TTT.c
    Fully working Tic Tac Toe, the computer chooses where to place its choice randomly

--- TTT.c
+++ TTT.c
@@ -4,262 +4,369 @@
 
 
 int main(){
-
-    printf("\n\n\n");
-
-    printf("    X    Y    Z \n"); 
-    printf("       |    |  \n");
-    printf("1      |    |  \n");
-    printf("   ----|----|----\n");
-    printf("2      |    |  \n");
-    printf("   ----|----|----\n");
-    printf("3      |    |  \n");
-    printf("       |    |  \n");
-
-    printf("\n\n\n");
-
-    int x1 = 2; 
-    int x2 = 2; 
-    int x3 = 2; 
-    int x4 = 2; 
-    int x5 = 2; 
-    int x6 = 2; 
-    int x7 = 2; 
-    int x8 = 2; 
-    int x9 = 2;
-    int i; 
-    char xPos;
+//canvas gets printed at the beginning of the game
+        printf("\n\n\n");
+        printf("      |    |  \n");
+        printf("   1  |  2 |  3\n");
+        printf("  ----|----|----\n");
+        printf("   4  |  5 |  6\n");
+        printf("  ----|----|----\n");
+        printf("   7  |  8 |  9\n");
+        printf("      |    |  \n");
+        printf("\n\n\n");
+   //variables here
+    int i, userChoice; 
     char choiceXO; 
-    int yPos; 
-    char arr[9] = {'-', '-', '-', '-', '-', '-', '-', '-', '-'};
-    char choices[2] = {'X','O'};
-    int size = sizeof(arr) / sizeof(arr[0]);
-    int sizeOfCh = sizeof(choices) / sizeof(choices[0]);
-    int whoStart = 0;
+    char arr[9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}; //this one is for the current positions and their status
+    int size = sizeof(arr) / sizeof(arr[0]); //Gets size of array?
+    int turns, whoStart = 0; //this governs turns
     int randomIndexCh;
     int randomIndex;
-    srand(time(0));
+    srand(time(0)); //initalizes the time
 
     printf("Would you like to go first (1) or the computer (0): ");
     scanf("%d", &whoStart);
     
+    turns = whoStart; //whoStart is weather you are X or O, turns is who's turn it is
 
-
+    //main for loop where shit goes down
     for (i = 0; i < 9; i++)
     {
-
-        
-
-      
+      //if the player chooses to go first
       if (whoStart == 1)
         {
-            printf("Enter your choice, ex. xy3 (This will place an X in the Y-3 position): ");
-            scanf(" %c %c %d", &choiceXO, &xPos, &yPos);   
-            whoStart = 0;      
-            
-        }
-        else{
+            //if it is the players turn
+           if(turns == 1){
+
+                printf("Enter your choice of position 1-9: ");
+                scanf("%d", &userChoice);   
+
+                arr[userChoice - 1] = 'X';
+
+                turns = 0; 
+                printf("Player Choie...");
+           }
+           //if it is the computers turn
+           else{
             randomIndex = rand() % size;
 
-            while (arr[randomIndex] != '-') //This will choose an availabe choice
+            while (arr[randomIndex] != ' ') //This will choose an availabe choice
             {
                 randomIndex = rand() % size; //This will choose the '-' if its not chosen yet
             }
             
-            randomIndexCh = rand() % sizeOfCh; //This chooses either X or O
+            arr[randomIndex] = 'O';   //This will set the chosen '-' to either X or O
+            turns = 1; 
 
-            arr[randomIndex] = choices[randomIndexCh];   //This will set the chosen '-' to either X or O
-            whoStart = 1; 
+            printf("Computer Choie...");
+
+           }
+                               
         }
+        //if the player chooses the computer goes first
+        else{
+            //if it is the computers turn
+            if(turns == 0){
 
+                randomIndex = rand() % size;
 
-        
-        
+                while (arr[randomIndex] != ' ') //This will choose an availabe choice
+                {
+                    randomIndex = rand() % size; //This will choose the '-' if its not chosen yet
+                }
+                
+                arr[randomIndex] = 'X';   //This will set the chosen '-' to either X or O
+                turns = 1; 
 
-/*---LINE 1 HOR---*/
+                printf("Computer Choie...");
+           }
+           //if it is the players turn
+           else{
+                printf("Enter your choice of position 1-9: ");
+                scanf("%d", &userChoice);   
 
-        if (choiceXO == 'x' && xPos == 'x' && yPos == 1 || choiceXO == 'X' && xPos == 'X' && yPos == 1 || arr[0] == 'X')
-        {
+                arr[userChoice - 1] = 'O';
 
-            x1 = 1; 
-            arr[randomIndex] = 'X'; 
-        }
-        else if (choiceXO == 'o' && xPos == 'x' && yPos == 1 || choiceXO == 'O' && xPos == 'O' && yPos == 1 || arr[0] == 'O')
-        {
-
-            x1 = 0; 
-            arr[randomIndex] = 'O'; 
-
-        }
-        else if (choiceXO == 'x' && xPos == 'y' && yPos == 1 || choiceXO == 'X' && xPos == 'Y' && yPos == 1 || arr[1] == 'X')
-        {
-
-            x2 = 1; 
-            arr[randomIndex] = 'X'; 
-
-        }
-        else if (choiceXO == 'o' && xPos == 'y' && yPos == 1 || choiceXO == 'O' && xPos == 'Y' && yPos == 1 || arr[1] == 'O')
-        {
-
-            x2 = 0; 
-            arr[randomIndex] = 'O'; 
-
+                turns = 0; 
+                printf("Player Choie...");
+           }
 
         }
-        else if (choiceXO == 'x' && xPos == 'z' && yPos == 1 || choiceXO == 'X' && xPos == 'Z' && yPos == 1 || arr[2] == 'X')
-        {
-
-            x3 = 1; 
-            arr[randomIndex] = 'X'; 
-
-        }
-        else if (choiceXO == 'o' && xPos == 'z' && yPos == 1 || choiceXO == 'O' && xPos == 'Z' && yPos == 1 || arr[2] == 'O')
-        {
-
-            x3 = 0; 
-            arr[randomIndex] = 'O'; 
-
-        }
-
-/*---LINE 2 HOR---*/
-        else if (choiceXO == 'x' && xPos == 'x' && yPos == 2 || choiceXO == 'X' && xPos == 'X' && yPos == 2 || arr[3] == 'X')
-        {
-
-            x4 = 1; 
-            arr[randomIndex] = 'X'; 
-
-        }
-        else if (choiceXO == 'o' && xPos == 'x' && yPos == 2 || choiceXO == 'O' && xPos == 'X' && yPos == 2 || arr[3] == 'O')
-        {
-
-            x4 = 0; 
-            arr[randomIndex] = 'O'; 
-
-        }
-        else if (choiceXO == 'x' && xPos == 'y' && yPos == 2 || choiceXO == 'X' && xPos == 'Y' && yPos == 2 || arr[4] == 'X')
-        {
-
-            x5 = 1; 
-            arr[randomIndex] = 'X'; 
-
-        }
-        else if (choiceXO == 'o' && xPos == 'y' && yPos == 2 || choiceXO == 'O' && xPos == 'Y' && yPos == 2 || arr[4] == 'O')
-        {
-
-            x5 = 0; 
-            arr[randomIndex] = 'O'; 
-
-        }
-        else if (choiceXO == 'x' && xPos == 'z' && yPos == 2 || choiceXO == 'X' && xPos == 'Z' && yPos == 2 || arr[5] == 'X')
-        {
-
-            x6 = 1; 
-            arr[randomIndex] = 'X'; 
-
-        }
-        else if (choiceXO == 'o' &&  xPos == 'z' && yPos == 2 || choiceXO == 'O' && xPos == 'Z' && yPos == 2 || arr[5] == 'O')
-        {
-
-            x6 = 0; 
-            arr[randomIndex] = 'O'; 
-
-        }
-
-/*---LINE 3 HOR---*/
-        else if (choiceXO == 'x' && xPos == 'x' && yPos == 3 || choiceXO == 'X' && xPos == 'X' && yPos == 3 || arr[6] == 'X')
-        {
-
-            x7 = 1; 
-            arr[randomIndex] = 'X'; 
-
-        }
-        else if (choiceXO == 'o' && xPos == 'x' && yPos == 3 || choiceXO == 'O' && xPos == 'X' && yPos == 3 || arr[6] == 'O')
-        {
-
-            x7 = 0; 
-            arr[randomIndex] = 'O'; 
-
-        }
-        else if (choiceXO == 'x' && xPos == 'y' && yPos == 3 || choiceXO == 'X' && xPos == 'Y' && yPos == 3 || arr[7] == 'X')
-        {
-
-            x8 = 1; 
-            arr[randomIndex] = 'X'; 
-
-        }
-        else if (choiceXO == 'o' && xPos == 'y' && yPos == 3 || choiceXO == 'O' && xPos == 'Y' && yPos == 3 || arr[7] == 'O')
-        {
-
-            x8 = 0; 
-            arr[randomIndex] = 'O'; 
-
-        }
-        else if (choiceXO == 'x' && xPos == 'z' && yPos == 3 || choiceXO == 'X' && xPos == 'Z' && yPos == 3 || arr[8] == 'X')
-        {
-
-            x9= 1; 
-            arr[randomIndex] = 'X'; 
-
-        }
-        else if (choiceXO == 'o' && xPos == 'z' && yPos == 3 || choiceXO == 'O' && xPos == 'Z' && yPos == 3 || arr[8] == 'O')
-        {
-
-            x9 = 0; 
-            arr[randomIndex] = 'O'; 
-
-        }
-
-
-        
-        
-        
-
+        //shitty canavs with choices here... took ages
         printf("\n\n\n");
-    
-
-        printf("    X    Y    Z \n"); 
-        printf("       |    |  \n");
-        printf("1%s%s%s%s%s%s",x1 == 1 ?"    X":(x1 == 0 ?"    O": "     ")," |",x2 == 1 ?"  X":(x2 == 0 ?"  O": "   ")," |",x3 == 1 ?"  X":(x3 == 0 ?"  O": "     "),"\n");
-        printf("   ----|----|----\n");
-        printf("2%s%s%s%s%s%s",x4 == 1 ?"    X":(x4 == 0 ?"    O": "     ")," |",x5 == 1 ?"  X":(x5 == 0 ?"  O": "   ")," |",x6 == 1 ?"  X":(x6 == 0 ?"  O": "     "),"\n");
-        printf("   ----|----|----\n");
-        printf("3%s%s%s%s%s%s",x7 == 1 ?"    X":(x7 == 0 ?"    O": "     ")," |",x8 == 1 ?"  X":(x8 == 0 ?"  O": "   ")," |",x9 == 1 ?"  X":(x9 == 0 ?"  O": "     "),"\n");
-        printf("       |    |  \n");
-
-
+        printf("        |      |  \n");
+        printf("     %c  |  %c   | %c \n", arr[0], arr[1], arr[2]);
+        printf("    ----|------|----\n");
+        printf("     %c  |  %c   | %c \n", arr[3], arr[4], arr[5]);
+        printf("    ----|------|----\n");
+        printf("     %c  |  %c   | %c \n", arr[6], arr[7], arr[8]);
+        printf("        |      |  \n");
         printf("\n\n\n");
 
 
-/*
-        X    Y    Z 
-           |    |  
-    1      |    |     
-       ----|----|----
-    2      |    |     
-       ----|----|----
-    3      |    |     
-           |    |  
-*/
 
 
+        /*BULLSHIT BELOW
+        /*FOR X HORIZANTAL*/
+        
+        /*FOR X FIRST ROW*/
+
+        if (arr[0] == 'X' && arr[1] == 'X' && arr[2] == 'X' )
+        {
+            if(whoStart == 1){
+                printf("\n\n\nTHE PLAYER WINS!!!");
+            }
+
+            if(whoStart == 0){
+                printf("\n\n\nTHE COMPUTER WINS :( \n\n\n");
+            }
+
+            exit(1); 
+        }
+        /*FOR X SECOND ROW*/
+
+        else if (arr[3] == 'X' && arr[4] == 'X' && arr[5] == 'X' )
+        {
+            if(whoStart == 1){
+                printf("\n\n\nTHE PLAYER WINS!!!");
+            }
+
+            if(whoStart == 0){
+                printf("\n\n\nTHE COMPUTER WINS :( \n\n\n");
+            }
+
+            exit(1); 
+        }
+        /*FOR X THIRD ROW*/
+
+        else if (arr[6] == 'X' && arr[7] == 'X' && arr[8] == 'X' )
+        {
+            if(whoStart == 1){
+                printf("\n\n\nTHE PLAYER WINS!!!");
+            }
+
+            if(whoStart == 0){
+                printf("\n\n\nTHE COMPUTER WINS :( \n\n\n");
+            }
+
+            exit(1); 
+        }
+        
+        /*FOR X VERTICAL*/
+
+        /*FOR X FIRST COLOUMN*/
+
+
+
+        if (arr[0] == 'X' && arr[3] == 'X' && arr[6] == 'X' )
+        {
+            if(whoStart == 1){
+                printf("\n\n\nTHE PLAYER WINS!!!");
+            }
+
+            if(whoStart == 0){
+                printf("\n\n\nTHE COMPUTER WINS :( \n\n\n");
+            }
+
+            exit(1); 
+        }
+
+        /*FOR X SECOND COLOUMN*/
+
+        else if (arr[1] == 'X' && arr[4] == 'X' && arr[7] == 'X' )
+        {
+            if(whoStart == 1){
+                printf("\n\n\nTHE PLAYER WINS!!!");
+            }
+
+            if(whoStart == 0){
+                printf("\n\n\nTHE COMPUTER WINS :( \n\n\n");
+            }
+
+            exit(1); 
+        }
+
+        /*FOR X THIRD COLOUMN*/
+
+        else if (arr[2] == 'X' && arr[5] == 'X' && arr[8] == 'X' )
+        {
+            if(whoStart == 1){
+                printf("\n\n\nTHE PLAYER WINS!!!");
+            }
+
+            if(whoStart == 0){
+                printf("\n\n\nTHE COMPUTER WINS :( \n\n\n");
+            }
+
+            exit(1); 
+        }
+
+        /*FOR X DIAGONAL*/
+
+        /*FOR X TOP LEFT TO BOTTOM RIGHT*/
+
+
+        if (arr[0] == 'X' && arr[4] == 'X' && arr[8] == 'X' )
+        {
+            if(whoStart == 1){
+                printf("\n\n\nTHE PLAYER WINS!!!");
+            }
+
+            if(whoStart == 0){
+                printf("\n\n\nTHE COMPUTER WINS :( \n\n\n");
+            }
+
+            exit(1); 
+        }
+        /*FOR X TOP RIGHT TO BOTTOM LEFT*/
+
+        if (arr[2] == 'X' && arr[4] == 'X' && arr[6] == 'X' )
+        {
+            if(whoStart == 1){
+                printf("\n\n\nTHE PLAYER WINS!!!");
+            }
+
+            if(whoStart == 0){
+                printf("\n\n\nTHE COMPUTER WINS :( \n\n\n");
+            }
+
+            exit(1); 
+        }
+        
+
+        /*FOR O HORIZANTAL*/
+        
+        /*FOR O FIRST ROW*/
+
+        if (arr[0] == 'O' && arr[1] == 'O' && arr[2] == 'O' )
+        {
+            if(whoStart == 1){
+                printf("\n\n\nTHE COMPUTER WINS :(");
+            }
+
+            if(whoStart == 0){
+                printf("\n\n\nTHE PLAYER WINS!!!");
+            }
+
+            exit(1); 
+        }
+
+
+         /*FOR O SECOND ROW*/
+
+        if (arr[3] == 'O' && arr[4] == 'O' && arr[5] == 'O' )
+        {
+            if(whoStart == 1){
+                printf("\n\n\nTHE COMPUTER WINS :(");
+            }
+
+            if(whoStart == 0){
+                printf("\n\n\nTHE PLAYER WINS!!!");
+            }
+
+            exit(1); 
+        }
+
+        /*FOR O THIRD ROW*/
+
+        if (arr[6] == 'O' && arr[7] == 'O' && arr[8] == 'O' )
+        {
+            if(whoStart == 1){
+                printf("\n\n\nTHE COMPUTER WINS :(");
+            }
+
+            if(whoStart == 0){
+                printf("\n\n\nTHE PLAYER WINS!!!");
+            }
+
+            exit(1); 
+        }
+
+        /*FOR O VERTICAL*/
+
+        /*FOR O FIRST COLOUMN*/
+
+        if (arr[0] == 'O' && arr[3] == 'O' && arr[6] == 'O' )
+        {
+            if(whoStart == 1){
+                printf("\n\n\nTHE COMPUTER WINS :(");
+            }
+
+            if(whoStart == 0){
+                printf("\n\n\nTHE PLAYER WINS!!!");
+            }
+
+            exit(1); 
+        }
+
+        /*FOR O SECOND COLOUMN*/
+
+        if (arr[1] == 'O' && arr[3] == 'O' && arr[7] == 'O' )
+        {
+            if(whoStart == 1){
+                printf("\n\n\nTHE COMPUTER WINS :(");
+            }
+
+            if(whoStart == 0){
+                printf("\n\n\nTHE PLAYER WINS!!!");
+            }
+
+            exit(1); 
+        }
+
+        /*FOR O THIRD COLOUMN*/
+
+        if (arr[2] == 'O' && arr[5] == 'O' && arr[8] == 'O' )
+        {
+            if(whoStart == 1){
+                printf("\n\n\nTHE COMPUTER WINS :(");
+            }
+
+            if(whoStart == 0){
+                printf("\n\n\nTHE PLAYER WINS!!!");
+            }
+
+            exit(1); 
+        }
+
+        /*FOR O DIAGONAL*/
+
+        /*FOR O TOP LEFT TO BOTTOM RIGHT*/
+        if (arr[0] == 'O' && arr[4] == 'O' && arr[8] == 'O' )
+        {
+            if(whoStart == 1){
+                printf("\n\n\nTHE COMPUTER WINS :(");
+            }
+
+            if(whoStart == 0){
+                printf("\n\n\nTHE PLAYER WINS!!!");
+            }
+
+            exit(1); 
+        }
+
+        /*FOR O TOP RIGHT TO BOTTOM LEFT*/
+
+        if (arr[2] == 'O' && arr[4] == 'O' && arr[6] == 'O' )
+        {
+            if(whoStart == 1){
+                printf("\n\n\nTHE COMPUTER WINS :(");
+            }
+
+            if(whoStart == 0){
+                printf("\n\n\nTHE PLAYER WINS!!!");
+            }
+
+            exit(1); 
+        }
 
 
 
     }
+    //if your a peanut and you tie with the computer...
+    printf("\n\n\nIt is a tie");
+
     
-
-
     return 0;
 
 }
-
-
-
-
-
-
-
-
-
-
-
